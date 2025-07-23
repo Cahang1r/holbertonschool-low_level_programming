742d333af5482eeb084f431baabc67e772f1b93f
Your Name <you@example.com>
2025-07-23 15:37:12 +0400

    ff

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void){
+    int n;
+
+	srand(time(0));  
+	n = rand() - RAND_MAX / 2;  
+    int last_digit = n % 10;
+    if (last_digit > 5){
+        printf("last digit is greater than 5");
+    }
+    else if(last_digit == 0){
+        printf("last digit is 0");
+    }
+    else {
+        printf("ess than 6 and not 0");
+    }
+}
