1623936466be5f78db66bd1e857bf2acb96feaee
Cahangir <11829@holbertonstudents.com>
2025-07-25 13:18:57 +0400

    ddd

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Description: Uses only the putchar function
+ * to print all lowercase letters followed by a newline.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char letter;
