586c510b171caaf09de5b96a81b4518d8bf63ace
Cahangir <11829@holbertonstudents.com>
2025-07-25 14:54:18 +0400

    ddd

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Prints numbers from 0 to 9
+ *
+ * Description: Uses a for loop to print numbers consecutively without spaces,
+ *              followed by a newline.
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	int n;
