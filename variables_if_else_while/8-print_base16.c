1f02073700cea2c569df22d280890bdf0f3e182e
Cahangir <11829@holbertonstudents.com>
2025-07-25 15:20:32 +0400

    ddd

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -3,9 +3,22 @@
 int main(void)
 {
     int number;
-    for (number = 0;number <= 16;number++)
-        putchar(number + '0');
+    for (number = 0; number <= 16; number++)
+    {
+        if (number < 10)
+        {
+            putchar(number + '0');
+
+        }
+        else
+        {
+            putchar(number / 10 + '0');
+            putchar(number % 10 + '0');
+
+        }
+
+    }
+       
     putchar('\n');
     return 0;
-    
 }
