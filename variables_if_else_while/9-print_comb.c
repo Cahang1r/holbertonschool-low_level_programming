964148d47469e422a37306995d2e20f4e8b900b0
Cahangir <11829@holbertonstudents.com>
2025-07-25 15:33:02 +0400

    ddd

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 9
+ *
+ * Description: Uses a for loop to print numbers consecutively without spaces,
+ *              followed by a newline.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + '0');
+        putchar(',');
+        putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
