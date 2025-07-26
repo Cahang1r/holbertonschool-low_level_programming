e5bf2ccf28947e654b024256c98efbeb7e188799
jahangirmammadli <memmedli0014@gmail.com>
2025-07-27 00:15:39 +0400

    test

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: the character to check (ASCII value)
+ *
+ * Return: 1 if letter (lowercase or uppercase), 0 otherwise
+ */
+int print_sign(int n)
+{
+    if (n>0)
+        return 1;
+    else if (n==0)
+        return 0;
+    else
+        return -1;
+    
+}
