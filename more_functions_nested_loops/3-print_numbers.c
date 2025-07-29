a3303a08c0cffbb3b93942bbe3dec3278b2aff4a
Cahangir <11829@holbertonstudents.com>
2025-07-29 13:10:20 +0400

    ddd

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,14 @@
 #include "main.h"
 
+/**
+ * print_numbers - prints the numbers 0 to 9 followed by a new line
+ */
 void print_numbers(void)
 {
-    char c;
+	char c;
 
-    for (c = 0; c <= 9; c++)
-        _putchar(c + '0');  /* convert number to ASCII digit */
+	for (c = 0; c <= 9; c++)
+		_putchar(c + '0'); /* convert number to ASCII digit */
 
-    _putchar('\n');
+	_putchar('\n');
 }
