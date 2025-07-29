d23cbdc4bd147e7505606453665cd2577fd77497
Cahangir <11829@holbertonstudents.com>
2025-07-29 14:53:27 +0400

    ddd

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,23 +1,26 @@
 #include "main.h"
 
+/**
+ * print_square - prints a square using '#' characters
+ * @size: the size of the square
+ */
 void print_square(int size)
 {
-    int i,j;
+	int i, j;
 
 	if (size <= 0)
 	{
 		_putchar('\n');
-		return;
 	}
-
-    for (i = 0; i < n; i++)
+	else
 	{
-		/* print spaces before the backslash */
-		for (j = 0; j < i; j++)
-			_putchar('#');
-
-		
-		_putchar('\n');
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
-
 }
