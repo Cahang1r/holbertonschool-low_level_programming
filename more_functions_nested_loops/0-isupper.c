601e2be9ead4b8a1d0f0abd33012754f0990aa78
Cahangir <11829@holbertonstudents.com>
2025-07-29 12:19:24 +0400

    ddd

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
  * _isupper - Checks if a character is uppercase
  * @c: The character to check
  *
- * Return: 1 if c is uppercase, 0 otherwise
+ * Return: 1 if c is uppercase,  0 otherwise
  */
  int main(void)
  {
@@ -12,6 +12,6 @@
     if (c >= 'A' && c <= 'Z')
         return 1;
     else
-        return 0;
+        return 0; 
 
  }
