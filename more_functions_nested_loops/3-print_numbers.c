4539b6d9daa9c16c82efac66ea11519007cc417d
Cahangir <11829@holbertonstudents.com>
2025-07-29 13:06:28 +0400

    ddd

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -3,8 +3,9 @@
 void print_numbers(void)
 {
     char c;
-    for ('c' = 0;'c' <= 9;'c'++)
-        _putchar(c);
-    _putchar('\n')
-		
+
+    for (c = 0; c <= 9; c++)
+        _putchar(c + '0');  // convert number to ASCII digit
+
+    _putchar('\n');  // missing semicolon added
 }
