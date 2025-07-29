d1da5038fdb3288647ac86127e4f7edc994fb328
Cahangir <11829@holbertonstudents.com>
2025-07-29 13:33:28 +0400

    ddd

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -5,14 +5,16 @@
  */
 void more_numbers(void)
 {
-    int i;
-    int j;
-    for (i = 0;i < 10;i++)
-    {
-        for (j = 0;j <= 14;j++)
-            	_putchar((j / 10) + '0');   /* print tens digit */
+	int i, j;
 
-			_putchar((j % 10) + '0');       /* print units digit */
-    }
-    _putchar('\n');
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar((j / 10) + '0');  /* print tens digit only if >= 10 */
+			_putchar((j % 10) + '0');      /* always print units digit */
+		}
+		_putchar('\n');  /* print newline after each line */
+	}
 }
