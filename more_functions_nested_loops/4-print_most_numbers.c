84d3bfdbf77e402490c78a2f8b43752222968d41
Cahangir <11829@holbertonstudents.com>
2025-07-29 13:17:56 +0400

    ddd

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers 0 to 9 followed by a new line
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = 0; c <= 9; c++)
+    {
+        if (c != 2,4)
+            _putchar(c + '0'); /* convert number to ASCII digit */
+    }
+	_putchar('\n');
+}
