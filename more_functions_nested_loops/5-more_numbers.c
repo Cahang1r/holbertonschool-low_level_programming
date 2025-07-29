b49cedf1ab312fee8fbca39575f7a0db1917eee1
Cahangir <11829@holbertonstudents.com>
2025-07-29 13:30:10 +0400

    ddd

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14, 10 times
+ */
+void more_numbers(void)
+{
+    int i;
+    int j;
+    for (i = 0;i < 10;i++)
+    {
+        for (j = 0;j <= 14;j++)
+            	_putchar((j / 10) + '0');   /* print tens digit */
+
+			_putchar((j % 10) + '0');       /* print units digit */
+    }
+    _putchar('\n')
+}
