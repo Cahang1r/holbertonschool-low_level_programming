161c6d51004fac6a02a631008eb85838a34d5e5c
cahangir <cahangir.com>
2025-08-02 16:16:52 +0400

    test4

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+    int length = 0;
+    int i;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+    length = (length + 1) / 2;
+
+    for (i = 0; i < length; i++)
+    {
+        _putchar(str[i]);
+    }
+	return (0);
+}
