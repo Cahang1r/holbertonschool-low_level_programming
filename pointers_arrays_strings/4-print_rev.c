f894d6ea94d0b07c08690e26b7fd92a63fc40e1d
cahangir <cahangir.com>
2025-08-02 14:25:22 +0400

    test4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,15 +8,18 @@
  */
 void print_rev(char *s)
 {
-	int a = 0;
+    int a = 0;
     int i;
 
-	while (s[a] != '\0')
-	{
-		a++;
-	}
+    while (s[a] != '\0')
+    {
+        a++;
+    }
+
     for (i = a - 1; i >= 0; i--)
+    {
         _putchar(s[i]);
-    
-	_putchar('\n');
+    }
+
+    _putchar('\n');
 }
