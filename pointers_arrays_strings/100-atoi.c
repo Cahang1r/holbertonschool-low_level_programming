b636f3abd57793c546ce6fb3ba0362895571f7a9
Cahangir <11829@holbertonstudents.com>
2025-08-04 11:50:08 +0400

    ddd

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,61 +1,23 @@
 #include "main.h"
-#include <limits.h>
 
 /**
- * _atoi - Converts a string to an integer.
- * @s: The string to convert.
+ * _atoi - changes a string to an int
+ * @s: the string to be changed
  *
- * Handles multiple '+' and '-' signs before digits,
- * stops parsing at the first non-digit after digits,
- * returns 0 if no digits found,
- * and prevents signed integer overflow and negation UB.
- *
- * Return: Converted integer.
+ * Return: the converted int
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, result = 0, found_digit = 0;
-	int digit;
-
-	while (s[i] != '\0')
-	{
-		if (s[i] == '-')
-			sign *= -1;
-		else if (s[i] == '+')
-			;
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			found_digit = 1;
-
-			/* Check overflow for positive */
-			if (sign == 1)
-			{
-				if (result > (INT_MAX - digit) / 10)
-					return (INT_MAX);
-			}
-			/* Check overflow for negative */
-			else
-			{
-				/* For INT_MIN, max positive value allowed is INT_MAX + 1 */
-				if (result > ((unsigned int)INT_MAX + 1U - digit) / 10)
-					return (INT_MIN);
-			}
-
-			result = result * 10 + digit;
-		}
-		else if (found_digit)
-			break;
-
-		i++;
-	}
-
-	if (sign == -1)
-	{
-		/* Handle INT_MIN without negation */
-		if (result == (unsigned int)INT_MAX + 1U)
-			return (INT_MIN);
-		return (-result);
-	}
-	return (result);
+    int i = 1;
+    unsigned int num = 0;
+    do
+    {
+        if (*s == '-')
+            i *= -1;
+        else if (*s >= '0' && *s <= '9')
+            num = num * 10 + (*s - '0');
+        else if (num > 0)
+            break;
+    } while (*s++);
+    return (num * i);
 }
