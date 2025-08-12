a8408b4d5d818ae45003d9540549ba40d399bfbc
cahangir <cahangir.com>
2025-08-12 11:13:32 +0400

    test

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -9,12 +9,12 @@
  */
 int _sqrt_helper(int n, int i)
 {
-    if (i * i == n)
-        return (i);
-    else if (i * i > n)
-        return (-1);
-    else
-        return (_sqrt_helper(n, i + 1));
+	if (i * i == n)
+		return (i);
+	else if (i * i > n)
+		return (-1);
+	else
+		return (_sqrt_helper(n, i + 1));
 }
 
 /**
@@ -25,10 +25,10 @@ int _sqrt_helper(int n, int i)
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return (-1);
-    else if (n == 0 || n == 1)
-        return (n);
-    else
-        return (_sqrt_helper(n, 1));
+	if (n < 0)
+		return (-1);
+	else if (n == 0 || n == 1)
+		return (n);
+	else
+		return (_sqrt_helper(n, 1));
 }
