838cf36db9da526e02eaf2169c45435cd9c05f1b
cahangir <cahangir.com>
2025-08-12 11:42:37 +0400

    test

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -9,12 +9,11 @@
  */
 int _is_prime_helper(int n, int i)
 {
-    if ( i * i > n)
+    if (i * i > n)
         return (1);
-    else if (n % i == 0)
-        return (0)
-    else
-        return (is_prime_number(n, i + 1));
+    if (n % i == 0)
+        return (0);
+    return (_is_prime_helper(n, i + 1));
 }
 
 /**
@@ -27,6 +26,5 @@ int is_prime_number(int n)
 {
     if (n <= 1)
         return (0);
-    else
-        return (is_prime_number(n, 2));
+    return (_is_prime_helper(n, 2));
 }
