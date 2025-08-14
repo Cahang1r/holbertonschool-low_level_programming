69fc5bb7ef875da5fd63e06fb0ab1f6c08727994
cahangir <cahangir.com>
2025-08-14 12:16:53 +0400

    test

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -17,11 +17,13 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
 
 	for (len1 = 0; s1[len1]; len1++)
 		;
+
 	for (len2 = 0; s2[len2]; len2++)
 		;
 
@@ -30,9 +32,14 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 
 	for (i = 0; i < len1; i++)
+	{
 		concat[i] = s1[i];
+	}
 
+	for (j = 0; j < len2; j++)
+	{
 		concat[i + j] = s2[j];
+	}
 
 	concat[len1 + len2] = '\0';
 
