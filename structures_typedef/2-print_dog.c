f3b07b065e8b57b66480bf5aee1b049c524b7017
Your Name <you@example.com>
2025-08-20 14:56:23 +0400

    test

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stddef.h>
+
+
+/**
+ * print_dog - prints a struct dog
+ * @d: pointer to the struct dog to print
+ *
+ * Description: If d is NULL, prints nothing.
+ *              If any string member is NULL, prints "(nil)" instead.
+ */
+void print_dog(struct dog *d)
+{
+    if (d == NULL)
+        return;
+
+    if (d->name == NULL)
+    {
+        printf("Name: %s\n", d->name,"(nil)");
+    }
+    printf("Age:%f\n",d->age);
+      if (d->owner == NULL)
+    {
+        printf("Owner: %s\n", d->owner,"(nil)");
+    }
+}
