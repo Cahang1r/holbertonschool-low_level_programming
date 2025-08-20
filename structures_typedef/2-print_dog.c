e42006ff66b2a52c94e1a048ce56f02d34bf49c3
Your Name <you@example.com>
2025-08-20 14:59:29 +0400

    test

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,6 +1,6 @@
 #include "dog.h"
-#include <stddef.h>
-
+#include <stdio.h>    // for printf
+#include <stddef.h>   // for NULL
 
 /**
  * print_dog - prints a struct dog
@@ -11,16 +11,18 @@
  */
 void print_dog(struct dog *d)
 {
-    if (d == NULL)
-        return;
+	if (d == NULL)
+		return;
 
-    if (d->name == NULL)
-    {
-        printf("Name: %s\n", d->name,"(nil)");
-    }
-    printf("Age:%f\n",d->age);
-      if (d->owner == NULL)
-    {
-        printf("Owner: %s\n", d->owner,"(nil)");
-    }
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+
+	printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
