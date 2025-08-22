6b868f4fd33e57cf9ac71db69cad668d7b1f4909
cahangir <cahangir.com>
2025-08-22 12:42:09 +0400

    test

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,23 +1,23 @@
-#include <stddef.h>
 #include "3-calc.h"
+#include <stddef.h> /* for NULL */
 
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-    while (ops[i].op)
-    {
-        if (ops[i].op[0] == s[0] && s[1] == '\0')
-            return (ops[i].f);
-        i++;
-    }
-    return (NULL);
+	while (ops[i].op)
+	{
+		if (ops[i].op[0] == s[0] && s[1] == '\0')
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
