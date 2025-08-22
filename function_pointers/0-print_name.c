d106fec007b21a540f234babc71c6210301de862
cahangir <cahangir.com>
2025-08-22 12:19:19 +0400

    test

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Calls a function to print a name
+ * @name: The name to be printed
+ * @f: A pointer to the function that prints the name
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+    if (name != NULL || f != NULL)
+        f(name);
+}
