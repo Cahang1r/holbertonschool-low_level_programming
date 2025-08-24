51a5f9989ad3280c9c43663816f47321f1af0dbe
cahangir <cahangir.com>
2025-08-24 12:15:20 +0400

    test

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_all - Prints anything, followed by a new line.
+ * @format: A string of format specifiers.
+ *
+ * Return: Nothing.
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	unsigned int i = 0;
+	char *str;
+	char *sep = "";
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		if (
+			format[i] == 'c' ||
+			format[i] == 'i' ||
+			format[i] == 'f' ||
+			format[i] == 's'
+		)
+		{
+			printf("%s", sep);
+			if (format[i] == 'c')
+				printf("%c", va_arg(args, int));
+			if (format[i] == 'i')
+				printf("%d", va_arg(args, int));
+			if (format[i] == 'f')
+				printf("%f", va_arg(args, double));
+			if (format[i] == 's')
+			{
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+			}
+			sep = ", ";
+		}
+		i++;
+	}
+
+	va_end(args);
+	printf("\n");
+}
